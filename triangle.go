3eb2e2c863e062d8383316ec885dc9df018c9d35
Ayan Akkassov <akkassov.ayan@gmail.com>
2022-07-10 19:16:43 +0600

    Implement Shape interface

--- triangle.go
+++ triangle.go
@@ -1,6 +1,17 @@
 package golang_united_school_homework
 
+import "math"
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+func (t Triangle) CalcPerimeter() float64 {
+	return t.Side * 3
+}
+
+func (t Triangle) CalcArea() float64 {
+	height := (math.Pow(3, 0.5) / 2) * t.Side
+	return height * t.Side
 }
