7eb3d84c45d0327aa5488c800e08a3489a797589
Ayan Akkassov <akkassov.ayan@gmail.com>
2022-07-10 19:56:20 +0600

    Realize ExtractByIndex method

--- box.go
+++ box.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 )
 
-var errorShapeOverflow = errors.New("shapeCapacity is full")
+var (
+	errorFullCapacity    = errors.New("shapeCapacity is full")
+	errorIndexOutOfRange = errors.New("index out of range")
+)
 
 // box contains list of shapes and able to perform operations on them
 type box struct {
@@ -25,7 +28,7 @@ func NewBox(shapesCapacity int) *box {
 func (b *box) AddShape(shape Shape) error {
 	shapesCount := len(b.shapes)
 	if shapesCount >= b.shapesCapacity {
-		return fmt.Errorf("could not add shape: %w", errorShapeOverflow)
+		return fmt.Errorf("could not add shape: %w", errorFullCapacity)
 	}
 	b.shapes = append(b.shapes, shape)
 	return nil
@@ -34,15 +37,30 @@ func (b *box) AddShape(shape Shape) error {
 // GetByIndex allows getting shape by index
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
-	panic("implement me")
-
+	shapesCount := len(b.shapes)
+	if i >= shapesCount {
+		return nil, fmt.Errorf("could not get by index: %w", errorIndexOutOfRange)
+	}
+	return b.shapes[i], nil
 }
 
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
-	panic("implement me")
+	shapesCount := len(b.shapes)
+	if i >= shapesCount {
+		return nil, fmt.Errorf("could not get by index: %w", errorIndexOutOfRange)
+	}
+	indexElement := b.shapes[i]
+	removeByIndex(b.shapes, i)
+	return indexElement, nil
+}
 
+// removeByIndex allows remove element by index from the slice and return new slice without this element
+func removeByIndex(s []Shape, index int) []Shape {
+	result := make([]Shape, 0)
+	result = append(result, s[:index]...)
+	return append(result, s[index+1:]...)
 }
 
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
